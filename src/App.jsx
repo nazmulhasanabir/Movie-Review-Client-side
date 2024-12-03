f462d6dd9cfa751ebf986229377c7064db6fe654
nazmulhasanabir <nazmulhasanabir2024@gmail.com>
2024-12-04 05:13:06 +0600

    Navbar finished

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <>
+   
+    <Navbar></Navbar>      
+      
+    </>
+  )
+}
+
+export default App
