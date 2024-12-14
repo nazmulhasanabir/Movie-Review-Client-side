e91a6588fc03ea57c9ab6a529ed27246e9ad4b8e
nazmulhasanabir <nazmulhasanabir2024@gmail.com>
2024-12-14 17:24:49 +0600

    favourite part

--- src/Fvrt.jsx
+++ src/Fvrt.jsx
@@ -1,13 +1,13 @@
 import { useContext, useEffect, useState } from "react";
 import Navbar from "./Components/Navbar";
 import { AuthContext } from "./Components/Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const Fvrt = () => {
   const { user } = useContext(AuthContext);
   const [favorites, setFavorites] = useState([]);
   const [loading, setLoading] = useState(true);
-  console.log(favorites);
-
+  // const email = user.email;
   useEffect(() => {
     if (user) {
       fetch(`http://localhost:5000/favorite/${user.email}`)
@@ -23,7 +23,25 @@ const Fvrt = () => {
         });
     }
   }, [user]);
-
+    const handleDelete = (email,_id) =>{
+      fetch(`http://localhost:5000/favorite/${email}`,{
+        method:"DELETE",
+      })
+      .then((res) => res.json())
+      .then((data)=>{
+        console.log(data);
+        if(data.deletedCount > 0){
+          Swal.fire({
+            title: "Deleted!",
+            text: "Deleted Successfully",
+            icon: "success",
+            confirmButtonText: "OK",
+          });
+          const remaining = favorites.filter((movie) => movie._id !== _id )
+          setFavorites(remaining)
+        }
+      })
+    }
   return (
     <div className="bg-gradient-to-br from-purple-900 via-black to-black text-white">
       <Navbar />
@@ -40,6 +58,7 @@ const Fvrt = () => {
               <p>Genre: {movie.genre}</p>
               <p>Duration: {movie.duration} minutes</p>
               <p>Rating: {movie.rating}</p>
+              <button onClick={()=>handleDelete(movie.email,movie._id)} className="btn">Delete</button>
             </div>
           ))
         ) }
